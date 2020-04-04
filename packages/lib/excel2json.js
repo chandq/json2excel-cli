5f02958b31fd92e5429bc8f81691e5fd93e3e9f3
chendq <deqiaochen@foxmail.com>
2020-04-04 22:08:44 +0800

    Code format

--- packages/lib/excel2json.js
+++ packages/lib/excel2json.js
@@ -10,14 +10,14 @@
  */
 
 // 读取多语言对照表的excel文件，并转换为JSON对象
-const excel2json = function(dirname, fileName = "hello.xlsx") {
-  let jsonArr = [];
-  const xlsx = require("xlsx"),
-    { utils } = xlsx;
-  const path = require("path");
+const excel2json = function (dirname, fileName = 'hello.xlsx') {
+  let jsonArr = []
+  const xlsx = require('xlsx'),
+    { utils } = xlsx
+  const path = require('path')
   // 获取数据
-  const workbook = xlsx.readFile(path.resolve(dirname, fileName));
-  var sheet_name_list = workbook.SheetNames;
+  const workbook = xlsx.readFile(path.resolve(dirname, fileName))
+  var sheet_name_list = workbook.SheetNames
   // console.time("sheet_to_json");
   // console.log(
   //   xlsx.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]], {
@@ -27,88 +27,88 @@ const excel2json = function(dirname, fileName = "hello.xlsx") {
   // console.timeEnd("sheet_to_json");
 
   // console.time("customlize generate JSON");
-  sheet_name_list.forEach(function(y) {
-    var worksheet = workbook.Sheets[y];
-    var headers = {};
-    var data = [];
+  sheet_name_list.forEach(function (y) {
+    var worksheet = workbook.Sheets[y]
+    var headers = {}
+    var data = []
     for (z in worksheet) {
-      if (z[0] === "!") continue;
+      if (z[0] === '!') continue
       //parse out the column, row, and value
-      var tt = 0;
+      var tt = 0
       for (var i = 0; i < z.length; i++) {
         if (!isNaN(z[i])) {
-          tt = i;
-          break;
+          tt = i
+          break
         }
       }
-      var col = z.substring(0, tt);
-      var row = parseInt(z.substring(tt));
-      var value = worksheet[z].v;
+      var col = z.substring(0, tt)
+      var row = parseInt(z.substring(tt))
+      var value = worksheet[z].v
 
       //store header names
       if (row == 1 && value) {
-        headers[col] = value;
-        continue;
+        headers[col] = value
+        continue
       }
 
-      if (!data[row]) data[row] = {};
-      data[row][headers[col]] = value;
+      if (!data[row]) data[row] = {}
+      data[row][headers[col]] = value
     }
     //drop those first two rows which are empty
-    data.shift();
-    data.shift();
-    jsonArr.push(data);
-  });
+    data.shift()
+    data.shift()
+    jsonArr.push(data)
+  })
   // console.timeEnd("customlize generate JSON");
-  return jsonArr;
-};
+  return jsonArr
+}
 
 // 将JSON对象格式化为不同语言的JSON对象
-const json2FormatLangObj = function(arr) {
+const json2FormatLangObj = function (arr) {
   let zh = {},
-    en = {};
-  const assignValue = function(originObj, zh_target_obj, en_target_obj, keysArr) {
-    !zh_target_obj && (zh_target_obj = {});
-    !en_target_obj && (en_target_obj = {});
+    en = {}
+  const assignValue = function (originObj, zh_target_obj, en_target_obj, keysArr) {
+    !zh_target_obj && (zh_target_obj = {})
+    !en_target_obj && (en_target_obj = {})
     if (keysArr.length > 1) {
-      !zh_target_obj[keysArr[0]] && (zh_target_obj[keysArr[0]] = {});
-      !en_target_obj[keysArr[0]] && (en_target_obj[keysArr[0]] = {});
-      let key = keysArr[0];
-      keysArr.shift();
-      assignValue(originObj, zh_target_obj[key], en_target_obj[key], keysArr);
+      !zh_target_obj[keysArr[0]] && (zh_target_obj[keysArr[0]] = {})
+      !en_target_obj[keysArr[0]] && (en_target_obj[keysArr[0]] = {})
+      let key = keysArr[0]
+      keysArr.shift()
+      assignValue(originObj, zh_target_obj[key], en_target_obj[key], keysArr)
     } else {
-      zh_target_obj[keysArr[0]] = originObj["简体中文"];
-      en_target_obj[keysArr[0]] = originObj["英文"];
+      zh_target_obj[keysArr[0]] = originObj['简体中文']
+      en_target_obj[keysArr[0]] = originObj['英文']
     }
-  };
+  }
   arr.forEach((v, i) => {
-    !v.key && (v.key = "xiaoyaozi-" + i);
-    if (v.key.includes(".")) {
-      assignValue(v, zh, en, v.key.split("."));
+    !v.key && (v.key = 'xiaoyaozi-' + i)
+    if (v.key.includes('.')) {
+      assignValue(v, zh, en, v.key.split('.'))
     } else {
-      zh[v.key] = v["简体中文"];
-      en[v.key] = v["英文"];
+      zh[v.key] = v['简体中文']
+      en[v.key] = v['英文']
     }
-  });
-  return { zh, en };
-};
+  })
+  return { zh, en }
+}
 
 // 将多语言JSON对象写入文件
-const writeToFile = function(dirname, data, fileName = "helloworld") {
-  const path = require("path");
-  const { writeFile } = require("fs").promises;
+const writeToFile = function (dirname, data, fileName = 'helloworld') {
+  const path = require('path')
+  const { writeFile } = require('fs').promises
   // zh语言对象写入文件
-  const zhPromise = function() {
-    return writeFile(path.resolve(dirname, `${fileName}_zh.json`), JSON.stringify(data.zh, null, 2));
-  };
+  const zhPromise = function () {
+    return writeFile(path.resolve(dirname, `${fileName}_zh.json`), JSON.stringify(data.zh, null, 2))
+  }
   // en语言对象写入文件
-  const enPromise = function() {
-    return writeFile(path.resolve(dirname, `${fileName}_en.json`), JSON.stringify(data.en, null, 2));
-  };
-  return Promise.all([zhPromise(), enPromise()]);
-};
+  const enPromise = function () {
+    return writeFile(path.resolve(dirname, `${fileName}_en.json`), JSON.stringify(data.en, null, 2))
+  }
+  return Promise.all([zhPromise(), enPromise()])
+}
 module.exports = {
   excel2json,
   json2FormatLangObj,
   writeToFile
-};
+}
--- packages/lib/json2excel.js
+++ packages/lib/json2excel.js
@@ -6,74 +6,74 @@
  * @depedence Node环境，安装xlsx模块
  */
 
-const json2Excel = function(dirname, zh, en, fileName) {
+const json2Excel = function (dirname, zh, en, fileName) {
   let zh_arr = [],
     en_arr = [],
-    zhen_xlsxArr = [];
-  let ObjKeys = "";
+    zhen_xlsxArr = []
+  let ObjKeys = ''
   /**
    * @description 资源文件JSON对象转换为对应的数组
    * @param {*} obj 中文或英文语言JSON对象en
    * @param {*} arr 保存中文或英文语言JSON数据的对象数组，格式为[{key: zh}]或[{key: en}]，key包含对象层级
    */
-  let obj2Arr = function(obj, arr) {
+  let obj2Arr = function (obj, arr) {
     for (let k in obj) {
-      if (!["number", "string"].includes(typeof obj[k])) {
-        ObjKeys += ObjKeys ? "." + k.toString() : "" + k.toString();
-        obj2Arr(obj[k], arr);
+      if (!['number', 'string'].includes(typeof obj[k])) {
+        ObjKeys += ObjKeys ? '.' + k.toString() : '' + k.toString()
+        obj2Arr(obj[k], arr)
       } else {
-        arr.push({ [ObjKeys ? `${ObjKeys}.${k}` : k.toString()]: obj[k] });
+        arr.push({ [ObjKeys ? `${ObjKeys}.${k}` : k.toString()]: obj[k] })
       }
     }
     if (ObjKeys) {
-      if (ObjKeys.includes(".")) {
-        let a = ObjKeys.split(".");
-        a.length = a.length - 1;
-        ObjKeys = a.join(".");
+      if (ObjKeys.includes('.')) {
+        let a = ObjKeys.split('.')
+        a.length = a.length - 1
+        ObjKeys = a.join('.')
       } else {
-        ObjKeys = "";
+        ObjKeys = ''
       }
     }
-  };
-  obj2Arr(zh, zh_arr);
-  obj2Arr(en, en_arr);
+  }
+  obj2Arr(zh, zh_arr)
+  obj2Arr(en, en_arr)
   for (let i = 0; i < zh_arr.length; i++) {
-    let key = Object.keys(zh_arr[i])[0];
-    let enValue = "";
+    let key = Object.keys(zh_arr[i])[0]
+    let enValue = ''
     for (let j = 0; j < en_arr.length; j++) {
-      let vkey = Object.keys(en_arr[j])[0];
+      let vkey = Object.keys(en_arr[j])[0]
       if (key === vkey) {
-        enValue = en_arr[j][vkey];
-        break;
+        enValue = en_arr[j][vkey]
+        break
       }
     }
     zhen_xlsxArr.push({
-      ["key"]: key,
-      ["简体中文"]: zh_arr[i][key],
-      ["英文"]: enValue || ""
-    });
+      ['key']: key,
+      ['简体中文']: zh_arr[i][key],
+      ['英文']: enValue || ''
+    })
   }
   // console.log("em: ", zh_arr, en_arr, zhen_xlsxArr);
-  const path = require("path");
-  const xlsx = require("xlsx"),
-    { utils } = xlsx;
-  const { writeFile } = require("fs").promises;
-  const workBook = utils.book_new();
+  const path = require('path')
+  const xlsx = require('xlsx'),
+    { utils } = xlsx
+  const { writeFile } = require('fs').promises
+  const workBook = utils.book_new()
   const workSheet = utils.json_to_sheet(zhen_xlsxArr, {
-    origin: "A1", // 从A1开始增加内容
-    header: ["key", "简体中文", "英文"],
+    origin: 'A1', // 从A1开始增加内容
+    header: ['key', '简体中文', '英文'],
     skipHeader: false // 跳过上面的标题行
-  });
+  })
 
   // 向工作簿中追加工作表
-  utils.book_append_sheet(workBook, workSheet, "helloWorld");
+  utils.book_append_sheet(workBook, workSheet, 'helloWorld')
   // 浏览器端和node共有的API,实际上node可以直接使用xlsx.writeFile来写入文件,但是浏览器没有该API
   const result = xlsx.write(workBook, {
-    bookType: "xlsx", // 输出的文件类型
-    type: "buffer", // 输出的数据类型
+    bookType: 'xlsx', // 输出的文件类型
+    type: 'buffer', // 输出的数据类型
     compression: true // 开启zip压缩
-  });
+  })
   // 写入文件
-  return writeFile(path.resolve(dirname, fileName), result);
-};
-module.exports = json2Excel;
+  return writeFile(path.resolve(dirname, fileName), result)
+}
+module.exports = json2Excel
